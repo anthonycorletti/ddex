c2b14b7c40109bfb449c0945c3a8facf7deec2cb
Anthony Corletti <anthcor@gmail.com>
2022-07-13 00:11:43 -0400

    initial tests and scaffolding

--- ddex/main.py
+++ ddex/main.py
@@ -1,7 +1,41 @@
-import os
+from typing import Dict, Optional
+from uuid import UUID
 
-os.environ["TZ"] = "UTC"
+
+class DDEXSchema:
+    def __init__(self) -> None:
+        pass
 
 
-def foo() -> str:
-    return "foo"
+class DDEX:
+    def __init__(self, id: UUID, ddex_schema: DDEXSchema) -> None:
+        pass
+
+    def to_xml(self, output_file: Optional[str] = None) -> str:
+        pass
+
+    def to_json(self, output_file: Optional[str] = None) -> str:
+        pass
+
+    def to_dict(self) -> Dict:
+        pass
+
+
+def _load_file(path: str) -> str:
+    pass
+
+
+def load_ddex_xsd_file(path: str) -> DDEXSchema:
+    pass
+
+
+def load_ddex_xml_file(path: str, schema: DDEXSchema) -> DDEX:
+    pass
+
+
+def load_ddex_json_file(path: str, schema: DDEXSchema) -> DDEX:
+    pass
+
+
+def load_ddex_dict(data: Dict, schema: DDEXSchema) -> DDEX:
+    pass
---
+++ tests/test_ddex.py
@@ -0,0 +1,26 @@
+def test_load_ddex_xsd_to_invalid_object() -> None:
+    assert False
+
+
+def test_load_ddex_xsd_to_valid_object() -> None:
+    assert False
+
+
+def test_load_ddex_xml_to_valid_object() -> None:
+    assert False
+
+
+def test_load_invalid_ddex_xml_to_object_fails() -> None:
+    assert False
+
+
+def test_ddex_object_to_xml() -> None:
+    assert False
+
+
+def test_ddex_object_to_json() -> None:
+    assert False
+
+
+def test_json_to_ddex_object() -> None:
+    assert False
