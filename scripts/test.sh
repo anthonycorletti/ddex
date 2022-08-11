2cd9a6ec264233322b6142e23489191bfe105390
Anthony Corletti <anthcor@gmail.com>
2022-08-11 01:35:57 -0400

    tests: test speed reduction needs more research

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,4 @@
 
 ./scripts/lint.sh
 
-# pytest --cov=ddex --cov=tests --cov-report=term-missing --cov-report=xml -o console_output_style=progress --disable-warnings --cov-fail-under=100 ${@}
-# TODO: get back to 100% coverage – pending network connection removal from tests
-pytest --cov=ddex --cov=tests --cov-report=term-missing --cov-report=xml -o console_output_style=progress --disable-warnings ${@}
+pytest --cov=ddex --cov=tests --cov-report=term-missing --cov-report=xml -o console_output_style=progress --disable-warnings --cov-fail-under=100 ${@}
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,6 @@
-import socket
-from typing import Any, Generator
-
 import pytest
 
-_original_connect = socket.socket.connect
 
-
-def patched_connect(*args: Any, **kwargs: Any) -> None:
-    raise Exception("No internet connection allowed for tests.")
-
-
-@pytest.fixture()
-def enable_network() -> Generator:
-    socket.socket.connect = _original_connect  # type: ignore
-    yield
-    socket.socket.connect = patched_connect  # type: ignore
-
-
-@pytest.fixture()
-def disable_network() -> Generator:
-    # TODO: add to tests to avoid network connection so that tests run faster
-    socket.socket.connect = patched_connect  # type: ignore
-    yield
-    socket.socket.connect = _original_connect  # type: ignore
+@pytest.fixture(scope="session", autouse=True)
+def _session_fixture() -> None:
+    pass
